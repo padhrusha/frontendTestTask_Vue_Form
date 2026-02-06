44f44e2e6a27b8c5442b03b5991ebb8adac60ebd
Dzmitry Padhrusha <dzmitry.padhrusha@krafteq.de>
2026-02-06 12:46:04 +0300

    initial commit, add packages, setup project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <h1>Vue App</h1>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
